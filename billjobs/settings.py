b4fb3f403ebb682e81e6e08674d385438e11be22
Lionel Chanson <lionel@soreia.com>
2014-04-01 12:15:59 +0200

    remove dev settings

--- billjobs/settings.py
+++ billjobs/settings.py
@@ -16,14 +16,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q)snhw!a-rd!lfb&t1_40+)zf#&faa%$jsr0a4ez1e!+&-87o+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -52,20 +44,10 @@ ROOT_URLCONF = 'billjobs.urls'
 WSGI_APPLICATION = 'billjobs.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
