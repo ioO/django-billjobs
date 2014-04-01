69bfc09e17e14f606caa6785786ff066f2045dd5
Lionel Chanson <lionel@soreia.com>
2014-04-01 14:46:43 +0200

    create Bill and BillLine object

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Bill(models.Model):
+
+    bill_date = models.DateField(auto_now_add=True)
+    line = models.ForeignKey('BillLine')
+
+class BillLine(models.Model):
+
+     SERVICES_CHOICES = (
+            (FT, 'Full Time'),
+            (PT, 'Part Time'),
+            (MD, 'Mid Time'),
+            )
+
+    service = models.CharField(max_length=2, choices=SERVICES_CHOICES, 
+                               default=Full Time)
+    quantity = models.SmallIntegerField()
+
