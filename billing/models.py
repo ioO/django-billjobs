7e227e09e9bd79a0f0020a73425415d5be6d0b97
Lionel Chanson <lionel@soreia.com>
2014-04-01 17:04:38 +0200

    add signals in pre save to compute line price

--- billing/models.py
+++ billing/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
 
 class Bill(models.Model):
 
     bill_date = models.DateField()
+
 
 class Service(models.Model):
 
@@ -15,9 +19,17 @@ class Service(models.Model):
         """ Return name as object representation """
         return self.name
 
+
 class BillLine(models.Model):
 
     bill = models.ForeignKey(Bill)
     service = models.ForeignKey(Service)
     quantity = models.SmallIntegerField(default=1)
-    total = models.FloatField()
+    total = models.FloatField(blank=True)
+
+
+@receiver(pre_save, sender=BillLine)
+def compute_total(sender, instance, **kwargs):
+        """ set total of line automatically """
+        if not instance.total:
+            instance.total = instance.service.price * instance.quantity
