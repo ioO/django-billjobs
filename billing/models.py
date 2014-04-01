7a629cc5668fe7387f710a849aa3927f6de143b0
Lionel Chanson <lionel@soreia.com>
2014-04-01 16:28:36 +0200

    add Service Model

--- billing/models.py
+++ billing/models.py
@@ -4,17 +4,16 @@ class Bill(models.Model):
 
     bill_date = models.DateField()
 
+class Service(models.Model):
+
+    reference = models.CharField(max_length=5)
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1024)
+    price = models.FloatField()
+
 class BillLine(models.Model):
 
     bill = models.ForeignKey(Bill)
     service = models.ForeignKey(Service)
     quantity = models.SmallIntegerField(default=1)
-    total = models.Float()
-
-class Service(models.Model):
-
-    reference = models.CharField(max_length=5)
-    name = models.CharField()
-    description = models.CharField()
-    price = models.FloatField()
-   
+    total = models.FloatField()
