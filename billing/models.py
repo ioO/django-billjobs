42b64119e33f06f198200e57e6ddbfbca7fab598
Lionel Chanson <lionel@soreia.com>
2014-04-01 15:13:30 +0200

    Change relation in billing models

--- billing/models.py
+++ billing/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 class Bill(models.Model):
 
-    bill_date = models.DateField(auto_now_add=True)
-    line = models.ForeignKey('BillLine')
+    bill_date = models.DateField()
 
 class BillLine(models.Model):
 
-     SERVICES_CHOICES = (
-            (FT, 'Full Time'),
-            (PT, 'Part Time'),
-            (MD, 'Mid Time'),
+    SERVICES_CHOICES = (
+            ('FT', 'Full Time'),
+            ('PT', 'Part Time'),
+            ('MD', 'Mid Time'),
             )
 
-    service = models.CharField(max_length=2, choices=SERVICES_CHOICES, 
-                               default=Full Time)
-    quantity = models.SmallIntegerField()
+    bill = models.ForeignKey(Bill)
+    service = models.CharField(max_length=2, choices=SERVICES_CHOICES,
+                               default='FT')
+    quantity = models.SmallIntegerField(default=1)
 
