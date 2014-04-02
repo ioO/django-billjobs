3a0fbfa953f86f3b877bf6e5cf9abe1eb8576b92
Lionel Chanson <lionel@soreia.com>
2014-04-02 11:29:26 +0200

    set bill number automatically

--- billing/models.py
+++ billing/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, pre_init
+import datetime
 
 
 class Bill(models.Model):
 
-    number = models.CharField(max_length=9, unique=True)
+    number = models.CharField(max_length=10, unique=True, blank=True)
     billing_date = models.DateField()
 
 
@@ -34,3 +35,21 @@ def compute_total(sender, instance, **kwargs):
         """ set total of line automatically """
         if not instance.total:
             instance.total = instance.service.price * instance.quantity
+
+@receiver(pre_save, sender=Bill)
+def define_number(sender, instance, **kwargs):
+    """ set bill number incrementally """
+
+    # only when we create record for the first time
+    if not instance.number:
+        today = datetime.date.today()
+        # get last id in base, we assume it's the last record
+        try:
+            last_record = sender.objects.latest('id')
+            #get last bill number and increment it
+            last_num = '%03d' % (int(last_record.number[-3:])+1)
+        # no Bill in db
+        except sender.DoesNotExist:
+            last_num = '001'
+
+        instance.number = 'F%s%s' % (today.strftime('%Y%m'), last_num)
