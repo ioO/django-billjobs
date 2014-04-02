d4942ff06d48f53c2de400c79d5c25c7a3f873f2
Lionel Chanson <lionel@soreia.com>
2014-04-02 11:48:46 +0200

    display billing address in user admin

--- billing/admin.py
+++ billing/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from billing.models import Bill, BillLine, Service
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from billing.models import Bill, BillLine, Service, UserProfile
 
 class BillLineInline(admin.TabularInline):
     model = BillLine
@@ -8,5 +10,15 @@ class BillLineInline(admin.TabularInline):
 class BillAdmin(admin.ModelAdmin):
     inlines = [BillLineInline]
 
+class UserProfileAdmin(admin.StackedInline):
+    model = UserProfile
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileAdmin, )
+
 admin.site.register(Bill, BillAdmin)
 admin.site.register(Service)
+
+# User have to be unregistered
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
