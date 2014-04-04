71f0617e0c5fae4ae9223b55d647048f63a95c34
Lionel Chanson <lionel@soreia.com>
2014-04-04 15:01:06 +0200

    generate a pdf from buffer and return it as response

--- billing/views.py
+++ billing/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 from django.contrib.auth.decorators import login_required
+from xhtml2pdf import pisa
+from io import BytesIO
 from models import Bill
 
 @login_required
 def generate_pdf(request, id):
     bill = Bill.objects.get(id=id)
-    # Create the HttpResponse object with the appropriate PDF headers.
-    #response = HttpResponse(content_type='application/pdf')
-    #response['Content-Disposition'] = 'attachment; filename="%s.pdf"' % bill.number
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="%s.pdf"' % bill.number
 
-    return render(request, 'generate_pdf.html', {'bill': bill})
+    # Create a buffer
+    buffer = BytesIO()
+    template = get_template('generate_pdf.html')
+    html  = template.render(Context(bill))
+    pisaStatus = pisa.CreatePDF(html, dest=buffer)
+    pdf = buffer.getvalue()
+    buffer.close()
+
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response.write(pdf)
+    return response
+
