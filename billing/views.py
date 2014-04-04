3326c921706369a5ad8cc53b7acb8f6cb0b47fa8
Lionel Chanson <lionel@soreia.com>
2014-04-04 14:18:52 +0200

    remove reportlab and use html as pdf template

--- billing/views.py
+++ billing/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from reportlab.pdfgen import canvas
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from models import Bill
@@ -8,17 +7,7 @@ from models import Bill
 def generate_pdf(request, id):
     bill = Bill.objects.get(id=id)
     # Create the HttpResponse object with the appropriate PDF headers.
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="%s.pdf"' % bill.number
+    #response = HttpResponse(content_type='application/pdf')
+    #response['Content-Disposition'] = 'attachment; filename="%s.pdf"' % bill.number
 
-    # Create the PDF object, using the response object as its "file."
-    p = canvas.Canvas(response)
-
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
-
-    # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
-    return response
+    return render(request, 'generate_pdf.html', {'bill': bill})
