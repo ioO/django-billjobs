1e29ca1015889a077afb447e8cedd2985422c7ec
Lionel Chanson <lionel@soreia.com>
2014-04-06 20:22:39 +0200

    display billing sender and customer info

--- billing/views.py
+++ billing/views.py
@@ -37,13 +37,35 @@ def generate_pdf(request, id):
     pdf.drawImage(settings.STATICFILES_DIRS[0]+'/logo-coworking.jpg', 0, height-75, width=138, height=75)
     # billing information
     lh = 15 #define a line height
-    pdf.setFillColorRGB(0.4,0.4,0.4)
+    pdf.setFillColorRGB(0.3,0.3,0.3)
     pdf.setFont("Helvetica-Bold", 14)
     pdf.drawRightString(width, height-lh, 'Facture');
     pdf.setFont("Helvetica-Bold", 10)
     pdf.drawRightString(width, height-2*lh, u'Numéro : %s' % bill.number)
     pdf.setFont("Helvetica", 10)
     pdf.drawRightString(width, height-3*lh, u'Date facturation : %s' % bill.billing_date.strftime('%d/%m/%Y'))
+
+    # define new height
+    nh = height - 90
+
+    # seller
+    pdf.setFillColorRGB(0.95,0.95,0.95)
+    # rect(x,y,width,height)
+    pdf.rect(0, nh-8*lh, width/2-40, 6.4*lh, fill=1)
+    # reset fill for text color
+    pdf.setFillColorRGB(0.3,0.3,0.3)
+    pdf.drawString(10, nh-lh, 'Emetteur')
+    pdf.drawString(20, nh-3*lh, 'Cowork\'in Montpellier')
+    pdf.drawString(20, nh-4*lh, '19 rue de l\'école de droit')
+    pdf.drawString(20, nh-5*lh, '34000 Montpellier')
+
+    # customer
+    pdf.drawString(width/2, nh-lh, 'Adressé à')
+    pdf.drawString(width/2+20, nh-3*lh, u'%s %s' % (bill.user.first_name, bill.user.last_name))
+    pdf.drawString(width/2+20, nh-4*lh, u'%s' % bill.user.userprofile.billing_address.replace('\n','<br />'))
+    pdf.setStrokeColorRGB(0,0,0)
+    # rect(x,y,width,height)
+    pdf.rect(width/2, nh-8*lh, width/2, 6.4*lh, fill=0)
 
     pdf.showPage()
     pdf.save()
