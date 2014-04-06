dce5cae853ccf811f92878cd8dbda11feb1ab206
Lionel Chanson <lionel@soreia.com>
2014-04-06 17:56:50 +0200

    define document size and margin

--- billing/views.py
+++ billing/views.py
@@ -2,6 +2,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import cm
 from core import settings
 from io import BytesIO
 from models import Bill
@@ -11,6 +14,14 @@ def generate_pdf(request, id):
     bill = Bill.objects.get(id=id)
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="%s.pdf"' % bill.number
+
+    pdf = canvas.Canvas(filename="test.pdf", pagesize=A4)
+    # define new 0,0 bottom left with cm as margin
+    pdf.translate(cm,cm)
+    # define document width and height with cm as margin
+    width, height = A4
+    width = width - 2*cm
+    height = height - 2*cm
 
     # Create a buffer
     buffer = BytesIO()
