64c6102c1d77ab4aa5afb3ef39c56f13c7c283ce
Lionel Chanson <lionel@soreia.com>
2014-04-07 12:42:30 +0200

    name url to pdf

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
 
-    url(r'^admin/generate_pdf/(?P<id>\d+)/$', 'billing.views.generate_pdf'),
+    url(r'^admin/generate_pdf/(?P<id>\d+)/$', 'billing.views.generate_pdf', 
+        name='generate_pdf'),
     url(r'^admin/', include(admin.site.urls)),
 )
