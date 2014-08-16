95d8722a7c926566f531ef360b3ac675b62861ed
Lionel Chanson <github@lionelchanson.fr>
2014-08-16 17:25:46 +0200

    define an url to app homepage

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'billjobs.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-
+    url(r'^$', 'billing.views.redirect_home'),
     url(r'^admin/generate_pdf/(?P<id>\d+)/$', 'billing.views.generate_pdf', 
         name='generate_pdf'),
     url(r'^admin/', include(admin.site.urls)),
