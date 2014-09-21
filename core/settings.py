41b1bb3c4818a23db4330a9fa6cc82db2a83f167
Lionel Chanson <github@lionelchanson.fr>
2014-09-21 15:22:39 +0200

    add LOCALE_PATH settings

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = (
+    '/home/ioo/dev/billjobs/locale',
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
