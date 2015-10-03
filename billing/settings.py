37e13a2a1b8a1889cb547edc014ad3f700f376e2
Lionel Chanson <github@lionelchanson.fr>
2015-10-03 15:31:00 +0200

    add settings file, define logo path for pdf

---
+++ billing/settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from core import settings
+import os.path
+
+# Set app base dir
+BILLING_BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+LOGO_PATH = BILLING_BASE_DIR + '/static/images/logo-default.jpg'
