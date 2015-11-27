724e8770487bddaacbb8a36aca4c5299c0d64474
Lionel Chanson <github@lionelchanson.fr>
2015-11-28 00:50:00 +0100

    clean core urls

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,8 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 admin.site.site_header = 'Coworking space administration'
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'billjobs.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', 'billing.views.redirect_home'),
-    url(r'^admin/generate_pdf/(?P<id>\d+)/$', 'billing.views.generate_pdf', 
-        name='generate_pdf'),
+urlpatterns = [
+    url(r'^billing/', include('billing.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
