38deca24215fca47c56fd0dd5ca03a45795ba93f
Lionel Chanson <github@lionelchanson.fr>
2015-11-28 02:02:59 +0100

    use billjobs name

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 admin.site.site_header = 'Coworking space administration'
 
 urlpatterns = [
-    url(r'^billing/', include('billing.urls')),
+    url(r'^billjobs/', include('billjobs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
