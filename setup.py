c6cbaad0c380fc6135ea3192af0c766c7ee967df
Lionel Chanson <github@lionelchanson.fr>
2015-12-11 23:55:58 +0100

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-billjobs',
-    version='0.2.5',
+    version='0.2.6',
     packages=['billjobs'],
     include_package_data=True,
     install_requires=[
