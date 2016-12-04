07199ffb94767e50fcc7fe44ab9bb125adda74cd
Lionel Chanson <github@lionelchanson.fr>
2016-12-04 16:43:28 +0100

    Change test name

--- billjobs/tests/tests_export_account_email.py
+++ billjobs/tests/tests_export_account_email.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
+from django.contrib.admin.sites import AdminSite
 from billjobs.admin import UserAdmin
 
 class EmailExportTestCase(TestCase):
     """ Tests for email account export """
 
-    def test_action_is_avaible(self):
+    def test_method_is_avaible(self):
         """ Test admin can select the action in dropdown list """
         self.assertTrue(hasattr(UserAdmin, 'export_email'))
+
