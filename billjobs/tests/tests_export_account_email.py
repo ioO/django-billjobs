a1e6da688cc41f0dc4a22e546d076358bd626990
Lionel Chanson <github@lionelchanson.fr>
2016-12-04 16:49:24 +0100

    Test modelAdmin actions is present

--- billjobs/tests/tests_export_account_email.py
+++ billjobs/tests/tests_export_account_email.py
@@ -9,3 +9,6 @@ class EmailExportTestCase(TestCase):
         """ Test admin can select the action in dropdown list """
         self.assertTrue(hasattr(UserAdmin, 'export_email'))
 
+    def test_method_is_model_admin_action(self):
+        """ Test method is an custom action for user admin """
+        self.assertTrue('export_email' in UserAdmin.actions)
