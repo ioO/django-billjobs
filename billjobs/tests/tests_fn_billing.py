f4aaf8c6d6fb994bb57dbafc45715c9d16f83fa6
Lionel Chanson <github@lionelchanson.fr>
2017-03-04 00:16:53 +0100

    Issue #23: Test link to user edit is in list

---
+++ billjobs/tests/tests_fn_billing.py
@@ -0,0 +1,21 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from billjobs.models import Bill, UserProfile
+
+class BillingAdminListViewTestCase(TestCase):
+    ''' Test billing model admin view '''
+
+    def test_coworker_name_link_to_user(self):
+        admin = User.objects.create_superuser(
+                username='admin', password='123', email='admin@billjobs.org',
+                first_name = 'Bill', last_name = 'Jobs')
+        profile = UserProfile(user=admin, billing_address='street')
+        profile.save()
+        invoice = Bill()
+        invoice.user = admin
+        invoice.save()
+        self.client.force_login(admin)
+        response = self.client.get('/admin/billjobs/bill/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,
+                '<td class="field-coworker_name"><a href="/admin/auth/user/1/change/">Bill Jobs</a></td>')
