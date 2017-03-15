f0781be5ca484100dd846e4a4520c36492ab7855
Lionel Chanson <github@lionelchanson.fr>
2017-03-15 15:09:40 +0100

    Add urls for rest url

--- billjobs/urls.py
+++ billjobs/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
-
+from django.conf.urls import url, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 urlpatterns = [
-    url(r'^generate_pdf/(?P<bill_id>\d+)$', views.generate_pdf, 
-        name='generate-pdf')
+    url(r'^generate_pdf/(?P<bill_id>\d+)$', views.generate_pdf,
+        name='generate-pdf'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     ]
