d8d3f01bf9fdbae8b5eed05d44b5e811c1af3de4
Lionel Chanson <github@lionelchanson.fr>
2017-03-24 21:59:38 +0100

    Remove rest_framework routers, add urlpattern for users api

--- billjobs/urls.py
+++ billjobs/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url, include
-from rest_framework import routers
 from . import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^generate_pdf/(?P<bill_id>\d+)$', views.generate_pdf,
         name='generate-pdf'),
-    url(r'^', include(router.urls)),
+    url(r'^users/$', views.UserAdmin.as_view(), name='users'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserAdminDetail.as_view(), name='user-detail'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     ]
