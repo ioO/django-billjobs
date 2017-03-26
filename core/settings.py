62d199d506a03a280338fa4832605bfdd526a55d
Lionel Chanson <github@lionelchanson.fr>
2017-03-26 14:01:12 +0200

    Add SessionAuthentication to allow browsing api with rest_frameworks html views

--- core/settings.py
+++ core/settings.py
@@ -103,6 +103,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser',
