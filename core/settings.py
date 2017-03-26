93614976d86b5825bb37d2ffd25031a013bdb294
Lionel Chanson <github@lionelchanson.fr>
2017-03-26 01:18:36 +0100

    Change default authentication to token

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ TEMPLATES = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser',
