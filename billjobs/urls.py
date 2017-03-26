e003aed10fadbd577355e1ee6935c89a3c5cc32b
Lionel Chanson <github@lionelchanson.fr>
2017-03-26 14:02:28 +0200

    Add rest_framework view to obtain auth token

--- billjobs/urls.py
+++ billjobs/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
@@ -6,5 +7,6 @@ urlpatterns = [
         name='generate-pdf'),
     url(r'^users/$', views.UserAdmin.as_view(), name='users'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserAdminDetail.as_view(), name='user-detail'),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', obtain_auth_token)
     ]
