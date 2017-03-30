6297dd1b3f4b81a57c47979a5cb00a7a1efc02fb
Lionel Chanson <github@lionelchanson.fr>
2017-03-30 16:56:23 +0200

    Name token auth url

--- billjobs/urls.py
+++ billjobs/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^users/$', views.UserAdmin.as_view(), name='users'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserAdminDetail.as_view(), name='user-detail'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_auth_token)
+    url(r'^api-token-auth/', obtain_auth_token, name='api-token-auth')
     ]
