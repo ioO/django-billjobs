361efbb0f6b2ed1a1238e97561f38e5567df3e6f
Lionel Chanson <github@lionelchanson.fr>
2017-03-30 17:28:15 +0200

    Test api status code for auth token

--- billjobs/tests/tests_user_admin_api.py
+++ billjobs/tests/tests_user_admin_api.py
@@ -7,15 +7,44 @@ from rest_framework.test import APIClient, APIRequestFactory, \
 from billjobs.views import UserAdmin, UserAdminDetail
 import json
 
-class UserAdminAPIStatus(TestCase):
+class APIStatusCode(TestCase):
     """
-    Test API response status code
+    Test user API response status code
     """
 
     fixtures=['test_api_user.yaml']
 
     def setUp(self):
-        pass
+        self.admin = User.objects.get(pk=1)
+        self.user = User.objects.get(pk=2)
+        self.client = APIClient()
+
+    def test_admin_auth_token(self):
+        """
+        Test status code is 200 when admin use correct credential
+        """
+        data = {'username': self.admin.username, 'password': 'jobs'}
+        response = self.client.post(reverse('api-token-auth'), data,
+                format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_auth_token(self):
+        """
+        Test status code is 200 when user user correct credential
+        """
+        data = {'username': self.user.username, 'password': 'jobs'}
+        response = self.client.post(reverse('api-token-auth'), data,
+                format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_user(self):
+        """
+        Test invalid user get 400
+        """
+        data = {'username': 'foo', 'password': 'bar'}
+        response = self.client.post(reverse('api-token-auth'), data,
+                format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 class APITokenAuthentication(TestCase):
     """
