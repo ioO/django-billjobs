d18b934409316ea80ae314abf2d10a1354d65289
Lionel Chanson <github@lionelchanson.fr>
2017-03-31 22:10:55 +0200

    Test user admin api status code is 200

--- billjobs/tests/tests_user_admin_api.py
+++ billjobs/tests/tests_user_admin_api.py
@@ -7,6 +7,25 @@ from rest_framework.test import APIClient, APIRequestFactory, \
 from billjobs.views import UserAdmin, UserAdminDetail
 import json
 
+class UserAdminAPIStatusCode(TestCase):
+    """
+    Test status code returned by endpoints
+    Status code related to permission are tested in APIPermission class
+    """
+
+    fixtures = ['test_api_user.yaml']
+
+    def setUp(self):
+        self.admin = User.objects.get(pk=1)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.admin)
+
+    def test_user_admin_get_is_200(self):
+        """
+        Test api user admin endpoints is HTTP_200_OK
+        """
+        response = self.client.get(reverse('user'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class UserAdminAPI(TestCase):
     """ Test User Admin API REST endpoint """
@@ -69,41 +88,7 @@ class UserAdminAPI(TestCase):
         response = self.client.put('/billjobs/users/1/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_admin_delete_user(self):
-        self.client.force_authenticate(user=self.admin)
-        response = self.client.delete('/billjobs/users/3/')
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    def test_anonymous_do_not_retrieve_user(self):
-        request = self.factory.get('/billjobs/users/')
-        view = UserAdmin.as_view()
-        response = view(request, pk=1)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_user_do_not_list_user(self):
-        request = self.factory.get('/billjobs/users/')
-        force_authenticate(request, user=self.user)
-        view = UserAdmin.as_view()
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_user_do_not_retrieve_user(self):
-        request = self.factory.get('/billjobs/users/')
-        force_authenticate(request, user=self.user)
-        view = UserAdminDetail.as_view()
-        response = view(request, pk=1)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_admin_create_user(self):
-        request = self.factory.post('/billjobs/users/',
-                json.dumps({
-                    'username': 'new_user',
-                    'email': 'new@jobs.org',
-                    'password': 'foobar'}),
-                content_type='application/json')
-        force_authenticate(request, user=self.admin)
-        view = UserAdmin.as_view()
-        response = view(request)
-        print(response.data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
+#    def test_admin_delete_user(self):
+#        self.client.force_authenticate(user=self.admin)
+#        response = self.client.delete('/billjobs/users/3/')
+#        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
