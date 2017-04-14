39a8c74c71dea1941815b8cdcff040c77ab00a6b
Lionel Chanson <github@lionelchanson.fr>
2017-04-14 11:41:48 +0200

    Remove UserAdminSerializer

--- billjobs/serializers.py
+++ billjobs/serializers.py
@@ -5,11 +5,6 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
-
-class UserAdminSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
         fields = '__all__'
         extra_kwargs = {
             'url': {'view_name': 'users-detail-api'}
