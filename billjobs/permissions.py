dc65920f52ca584608633cc511590b41b590f79e
Lionel Chanson <github@lionelchanson.fr>
2017-04-21 12:06:37 +0200

    Create permission for admin and user can access GET, PUT, DELETE method, user can access is instance only

--- billjobs/permissions.py
+++ billjobs/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from rest_framework.compat import is_authenticated
 
 class CustomUserAPIPermission(permissions.BasePermission):
     """
@@ -21,3 +22,28 @@ class CustomUserAPIPermission(permissions.BasePermission):
             return True
         # all other methods are accepted to allow 405 response
         return True
+
+class CustomUserDetailAPIPermission(permissions.BasePermission):
+    """
+    Set custom permission for user detail API
+
+    * GET, PUT, DELETE   :
+        * admin can access all users instance
+        * current user only his instance
+        * public is forbidden
+    """
+    def has_permission(self, request, view):
+        """
+        Give permission for admin or user to access API
+        """
+        return (
+                request.user and
+                request.user.is_staff or
+                is_authenticated(request.user)
+                )
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Compare User instance in request is equal to User instance in obj
+        """
+        return request.user.is_staff or obj == request.user
