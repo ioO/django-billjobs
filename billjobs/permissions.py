a7a124e8c02dafa83710c8afcae5597fc78bf42b
Lionel Chanson <github@lionelchanson.fr>
2017-04-21 12:05:24 +0200

    Fix UserAPI permissions

--- billjobs/permissions.py
+++ billjobs/permissions.py
@@ -2,16 +2,22 @@ from rest_framework import permissions
 
 class CustomUserAPIPermission(permissions.BasePermission):
     """
-    Define custom permission for UserAPI and UserDetailAPI
+    Set custom permission for UserAPI
 
-    GET : only accessible by admin
-    POST: public
+    * GET   : only accessible by admin
+    * POST  : is public, everyone can create a user
     """
 
     def has_permission(self, request, view):
+        """
+        Define permission based on request method
+        """
         if request.method == 'GET':
+            # admin only
             return request.user and request.user.is_staff
 
         elif request.method == 'POST':
+            # is public
             return True
+        # all other methods are accepted to allow 405 response
         return True
