f7fda787ffe5fa6ee0cf0692673b873d38f6017e
Lionel Chanson <github@lionelchanson.fr>
2017-05-24 16:27:22 +0200

    feat(permissions): add CustomGroupAPIPermission, manage GET access permission

--- billjobs/permissions.py
+++ billjobs/permissions.py
@@ -1,6 +1,25 @@
 from rest_framework import permissions
 from rest_framework.compat import is_authenticated
 
+class CustomGroupAPIPermission(permissions.BasePermission):
+    """
+    Set custom permission for GroupAPI
+
+    * GET   : accessible by admin and user
+    * POST  : only accessible by admin
+    """
+
+    def has_permission(self, request, view):
+        """
+        Define permission based on request
+        """
+        if request.method == 'GET':
+            return (
+                request.user and
+                request.user.is_staff or
+                is_authenticated(request.user)
+                )
+
 class CustomUserAPIPermission(permissions.BasePermission):
     """
     Set custom permission for UserAPI
