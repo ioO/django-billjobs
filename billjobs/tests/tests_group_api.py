d505a99600b25308b9b84f4b689965451fe09b3e
Lionel Chanson <github@lionelchanson.fr>
2017-06-06 14:29:35 +0200

    test(group): test group api DELETE method is not allowed for logged user

--- billjobs/tests/tests_group_api.py
+++ billjobs/tests/tests_group_api.py
@@ -45,3 +45,9 @@ class GroupAPIUserStatusCode(GenericAPIStatusCode):
         super().status_code_is(
                 'PUT', self.url, self.data, status.HTTP_405_METHOD_NOT_ALLOWED)
 
+    def test_api_group_delete_is_405(self):
+        """
+        Test group api with DELETE method return HTTP_405_METHOD_NOT_ALLOWED
+        """
+        super().status_code_is(
+                'DELETE', self.url, None, status.HTTP_405_METHOD_NOT_ALLOWED)
