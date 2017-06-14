cba1b890951e6b40ace7d3febc7261ff57270971
Lionel Chanson <github@lionelchanson.fr>
2017-06-14 15:22:53 +0200

    test(group-api): test response status code returned to an authenticated user with all HTTP method for /groups endpoint

--- billjobs/tests/tests_group_api.py
+++ billjobs/tests/tests_group_api.py
@@ -99,3 +99,53 @@ class AnonymousGroupDetailAPITest(GenericAPITest):
     def test_group_api_content(self):
         self.content_is()
 
+class UserGroupAPITest(GenericAPITest):
+    """
+    Tests status code and response content returned by /groups endpoint for
+    authenticated user.
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.url = reverse('groups-api')
+        self.force_authenticate(user=self.user)
+        self.data = {
+                'create': {'name': 'group name'},
+                'update': {'name': 'new name'}
+                }
+        self.expected_status = {
+                'GET': 200,
+                'POST': 403,
+                'PUT': 403,
+                'DELETE': 403,
+                'HEAD': 403,
+                'OPTIONS': 403,
+                'PATCH': 403,
+                }
+        self.expected_content = {
+                'GET': {
+                    'detail': 'Authentication credentials were not provided.'},
+                'POST': {
+                    'detail': 'Authentication credentials were not provided.'},
+                'PUT': {
+                    'detail': 'Authentication credentials were not provided.'},
+                'DELETE': {
+                    'detail': 'Authentication credentials were not provided.'},
+                'HEAD': {
+                    'detail': 'Authentication credentials were not provided.'},
+                'OPTIONS': {
+                    'detail': 'Authentication credentials were not provided.'},
+                'PATCH': {
+                    'detail': 'Authentication credentials were not provided.'},
+                }
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_group_api_status_code(self):
+        self.status_code_is()
+
+    def test_group_api_content(self):
+        pass
+        #self.content_is()
+
