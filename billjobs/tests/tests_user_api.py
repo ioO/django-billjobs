1f25dc7cf27efb825e5536da2ed2d82709995c54
Lionel Chanson <github@lionelchanson.fr>
2017-06-30 11:34:45 +0200

    test(user-api): test /users endpoint with all HTTP method for admin

--- billjobs/tests/tests_user_api.py
+++ billjobs/tests/tests_user_api.py
@@ -238,3 +238,110 @@ class UserUserDetailAPITest(GenericAPITest):
         self.status_code_is()
         self.content_is()
 
+class AdminUserAPITest(GenericAPITest):
+    """
+    Tests status code and response content returned by /users endpoint for 
+    admin.
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+        super().setUp()
+        self.url = reverse('users-api')
+        self.force_authenticate(user=self.admin)
+        self.data = {
+                'create': {
+                    'username': 'foo',
+                    'password': 'bar',
+                    'email': 'foo@bar.org'
+                    },
+                'update': {'username': 'bar'}
+                }
+        self.expected_status = {
+                'GET': 200,
+                'POST': 201,
+                'PUT': 405,
+                'DELETE': 405,
+                'HEAD': 200,
+                'OPTIONS': 200,
+                'PATCH': 405,
+                }
+        self.expected_length = {
+                'GET': 5,
+                'POST': 1
+                }
+        self.expected_content = {
+                'GET': [
+                    collections.OrderedDict({
+                        'url': 'http://testserver/billjobs/api/1.0/users/1/',
+                        'username': 'superuser',
+                        'email': 'superuser@billjobs.org',
+                        'password': 'pbkdf2_sha256$30000$J1xYbnHgWv7g$LcXs7lgyFtmoDKUn0vzi15jWsLCSDgr6ikOj2/1uNl4=',
+                        'is_staff': True,
+                        'is_superuser': True,
+                        'groups': []
+                        }),
+                    collections.OrderedDict({
+                        'url': 'http://testserver/billjobs/api/1.0/users/2/',
+                        'username': 'admin',
+                        'email': 'admin@billjobs.org',
+                        'is_staff': True,
+                        'is_superuser': False,
+                        'groups': ['http://testserver/billjobs/api/1.0/groups/1/']
+                        }),
+                    collections.OrderedDict({
+                        'url': 'http://testserver/billjobs/api/1.0/users/3/',
+                        'username': 'bill',
+                        'email': 'bill@billjobs.org',
+                        'is_staff': False,
+                        'is_superuser': False,
+                        'groups': [
+                            'http://testserver/billjobs/api/1.0/groups/2/',
+                            'http://testserver/billjobs/api/1.0/groups/3/'
+                            ]
+                        }),
+                    collections.OrderedDict({
+                        'url': 'http://testserver/billjobs/api/1.0/users/4/',
+                        'username': 'steve',
+                        'email': 'steve@billjobs.org',
+                        'is_staff': False,
+                        'is_superuser': False,
+                        }),
+                    collections.OrderedDict({
+                        'url': 'http://testserver/billjobs/api/1.0/users/5/',
+                        'groups': [],
+                        'is_superuser': False,
+                        'username': 'no-group',
+                        'email': 'no-group@billjobs.org',
+                        'is_staff': False,
+                        'is_active': True,
+                        }),
+                    ],
+                'POST': {
+                    'url': 'http://testserver/billjobs/api/1.0/users/6/',
+                    'username': 'foo',
+                    'password': 'bar',
+                    'email': 'foo@bar.org',
+                    'groups': [],
+                    'is_staff': False,
+                    'is_active': True
+                    },
+                'PUT': self.error_message['405_PUT'],
+                'DELETE': self.error_message['405_DELETE'],
+                'HEAD': [],
+                'OPTIONS': {},
+                'PATCH': self.error_message['405_PATCH'],
+                }
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_user_api_status_code(self):
+        self.status_code_is()
+
+    def test_user_api_content_length(self):
+        self.content_len_is()
+
+    def test_user_api_content(self):
+        self.content_is()
+
