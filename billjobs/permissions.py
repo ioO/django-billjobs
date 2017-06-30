49d63d652214ee49d2ba180bedf6f99804430098
Lionel Chanson <github@lionelchanson.fr>
2017-06-30 12:05:49 +0200

    feat(permission): change permission for user detail view

--- billjobs/permissions.py
+++ billjobs/permissions.py
@@ -110,7 +110,7 @@ class CustomUserDetailAPIPermission(permissions.BasePermission):
                 request.user.is_staff or
                 is_authenticated(request.user)
                 )
-        return False
+        return request.user and request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
         """
