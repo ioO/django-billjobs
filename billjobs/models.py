b2cc57c228ad2cea9ad48410dcf3e97e8c74228f
Lionel Chanson <github@lionelchanson.fr>
2017-10-27 18:36:58 +0200

    fix: remove create_auth_token from billjobs model

--- billjobs/models.py
+++ billjobs/models.py
@@ -141,9 +141,3 @@ def set_bill_amount(sender, instance, **kwargs):
 
     if sender is not Bill:
         bill.save()
-
-@receiver(post_save, sender=User)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-
