e0804fbabe9fd2fc3c4b509ba8e2eef600b152ab
Lionel Chanson <github@lionelchanson.fr>
2017-11-09 17:43:59 +0100

    feat(signup): add url for signup success view

--- billjobs/urls.py
+++ billjobs/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
         url(r'^generate_pdf/(?P<bill_id>\d+)$', views.generate_pdf,
             name='generate-pdf'),
-        url(r'^signup/$', views.signup, name='billjobs_signup')
+        url(r'^signup/$', views.signup, name='billjobs_signup'),
+        url(r'^signup-success/$', views.signup_success,
+            name='billjobs_signup_success')
         ]
