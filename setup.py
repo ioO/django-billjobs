80a7bbc915fca65b0f4999f36c64a1c174bc0aca
Lionel Chanson <github@lionelchanson.fr>
2019-03-06 09:16:34 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-billjobs',
-    version='0.8.1',
+    version='0.8.2',
     packages=['billjobs'],
     include_package_data=True,
     install_requires=[
