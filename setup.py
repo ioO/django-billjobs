4e0f5c60714cf5910ab395e2b92a2b61a890e0ce
Thomas Maurin <thomas@maurin.io>
2021-02-02 00:21:58 +0100

    [setup.py] Bump version to 0.9.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-billjobs',
-    version='0.8.2',
+    version='0.9.0',
     packages=['billjobs'],
     include_package_data=True,
     python_requires='>=3.6',
