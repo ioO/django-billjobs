1895f38597e6c7ca2dfe32dd60e2214c6001ceda
Thomas Maurin <thomas@maurin.io>
2022-11-21 09:10:22 +0100

    fix urls deprecation

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from core import settings
 from billjobs.admin import admin_site
 admin_site.site_header = 'Coworking space administration'
 
 urlpatterns = [
-    re_path(r'^billjobs/', include('billjobs.urls')),
+    path('billjobs/', include('billjobs.urls')),
     path('admin/', admin_site.urls),
 ]
 
