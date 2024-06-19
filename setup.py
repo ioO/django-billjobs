4aadb62d7ceda809163821e1ac31c02628807257
Thomas Maurin <thomas@maurin.io>
2024-06-20 00:07:05 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-billjobs',
-    version='0.11.1',
+    version='0.12.0',
     packages=['billjobs'],
     include_package_data=True,
     python_requires='>=3.10',
